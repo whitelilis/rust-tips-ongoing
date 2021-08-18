b4d923e3fcc16e5713d19a5fcb8cfebad7cd8e05
whitelilis <whitelilis@gmail.com>
2021-08-18 10:06:28 +0800

    add cant_borrow_data_from_ref_as_mutable.rs

---
+++ src/cant_borrow_data_from_ref_as_mutable.rs
@@ -0,0 +1,35 @@
+use std::collections::{HashMap, HashSet};
+use std::sync::{Arc, Mutex};
+
+#[derive(Debug, Default, Clone)]
+pub struct Manager {
+    pub job_task_map: HashMap<String, HashSet<String>>, // only id -> id
+}
+
+#[allow(dead_code)]
+pub struct Task {
+    job_id: String,
+    task_id: String,
+}
+
+#[allow(clippy::or_fun_call)]
+fn _hash_check_exists_or_insert(arc: Arc<Mutex<Manager>>, task: &Task) {
+    // WRONG
+    /*  can't borrow data from ref as mutable
+    arc.lock()
+        .unwrap()
+        .job_task_map
+        .get(task.job_id.clone().as_ref())
+        .get_or_insert(&HashSet::new())
+        .insert(task.task_id.clone());
+    */
+
+    // RIGHT
+
+    arc.lock()
+        .unwrap()
+        .job_task_map
+        .entry(task.job_id.clone())
+        .or_insert(HashSet::new())
+        .insert(task.task_id.clone());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+mod cant_borrow_data_from_ref_as_mutable;
 mod mv_between_collection;
 
 fn main() {
+    // a 'mut' ref,  means a unique ref,
     println!("Hello, world!");
 }
