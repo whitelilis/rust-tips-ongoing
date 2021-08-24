2b4c8f3ce82150ddc1d4b01f3d1506e91159bb10
whitelilis <whitelilis@gmail.com>
2021-08-24 08:32:09 +0800

    add doc template missing files

---
+++ src/add_docs.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod test {
+
+    ///
+    /// This function is just for test, wizard just add this doc.
+    /// # Most time, H1 is enough
+    /// ```rust,no_run
+    /// fn test1() -> u8{
+    ///     8;
+    /// }
+    /// ```
+
+    #[test]
+    fn add_doc_for_this() {
+        assert_eq!(3 * 5, 15);
+    }
+}
